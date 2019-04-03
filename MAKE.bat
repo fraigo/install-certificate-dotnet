9c30cf6960f58ab03c089262fdcd3dc4f2bb5fe5
Francisco <Francisco@stanPC.tbadigital.local>
2019-04-03 09:33:40 -0700

    Initial commit

---
+++ MAKE.bat
@@ -0,0 +1,4 @@
+C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /t:winexe /out:InstallCert.exe InstallCert.cs
+C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /t:winexe /out:DeleteCert.exe DeleteCert.cs
+
+pause

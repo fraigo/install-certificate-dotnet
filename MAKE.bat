d0c5657c23607373bd8b4ffb397c24f44562038d
Francisco <Francisco@stanPC.tbadigital.local>
2019-04-03 10:48:03 -0700

    Using .NET Framework 2.0 for compiling

--- MAKE.bat
+++ MAKE.bat
@@ -1,4 +1,4 @@
-C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /t:winexe /out:InstallCert.exe InstallCert.cs
-C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /t:winexe /out:DeleteCert.exe DeleteCert.cs
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe /t:winexe /out:InstallCert.exe InstallCert.cs
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe /t:winexe /out:DeleteCert.exe DeleteCert.cs
 
 pause

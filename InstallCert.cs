3ac2c3e0c07f9838bf7a4e25d4b285473f1ec9c0
Francisco <Francisco@stanPC.tbadigital.local>
2019-04-03 09:38:48 -0700

    Updated format

--- InstallCert.cs
+++ InstallCert.cs
@@ -4,25 +4,25 @@ using System.Security.Cryptography.X509Certificates;
 namespace AddCertToRootStore {
     class Program {
         static void Main (string[] args) {
-		String file = @"rootCA.pem";
-		if (args.Length >0)
-		{
-			file = args[1];
-		}
-		X509Store store = new X509Store (StoreName.Root,
-					     StoreLocation.CurrentUser);
-		store.Open (OpenFlags.ReadWrite);
-		X509Certificate2Collection collection = new X509Certificate2Collection();
-		X509Certificate2 cert = new X509Certificate2 (file);
-		byte[] encodedCert = cert.GetRawCertData();
-		if (store.Certificates.Contains(cert)){
-			//Console.WriteLine ("The certificate already exists");
-		}else{
-			//Console.WriteLine ("The certificate will be added to the Root...");
-			store.Add (cert);
-		}
-		store.Close ();
-		System.Environment.Exit(0);
+        String file = @"rootCA.pem";
+        if (args.Length >0)
+        {
+            file = args[1];
+        }
+        X509Store store = new X509Store (StoreName.Root,
+                         StoreLocation.CurrentUser);
+        store.Open (OpenFlags.ReadWrite);
+        X509Certificate2Collection collection = new X509Certificate2Collection();
+        X509Certificate2 cert = new X509Certificate2 (file);
+        byte[] encodedCert = cert.GetRawCertData();
+        if (store.Certificates.Contains(cert)){
+            //Console.WriteLine ("The certificate already exists");
+        }else{
+            //Console.WriteLine ("The certificate will be added to the Root...");
+            store.Add (cert);
+        }
+        store.Close ();
+        System.Environment.Exit(0);
         }
     }
 }
